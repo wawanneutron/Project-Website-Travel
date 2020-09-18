e754a37185294b9add5f3a0dfb9157db539a8aab
wawanneutron <wawanneutron1331@gmail.com>
2020-09-18 23:22:03 +0700

    Report Data Statistik

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\TravelPackage;
+use App\Transaction;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index(Request $request)
     {
-        return view('pages.admin.dashboard');
+        return view('pages.admin.dashboard', [
+            'travel_package' => TravelPackage::count(),
+            'transactions' => Transaction::count(),
+            'transaction_pending' => Transaction::where('transaction_status', 'PENDING')->count(),
+            'transaction_success' => Transaction::where('transaction_status', 'SUCCESS')->count(),
+        ]);
     }
 }
--- resources/views/pages/admin/dashboard.blade.php
+++ resources/views/pages/admin/dashboard.blade.php
@@ -20,7 +20,7 @@
           <div class="row no-gutters align-items-center">
             <div class="col mr-2">
               <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Paket Travel</div>
-              <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
+            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $travel_package }}</div>
             </div>
             <div class="col-auto">
               <i class="fas fa-luggage-cart fa-2x text-gray-300"></i>
@@ -37,7 +37,7 @@
           <div class="row no-gutters align-items-center">
             <div class="col mr-2">
               <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Transaksi</div>
-              <div class="h5 mb-0 font-weight-bold text-gray-800">14</div>
+              <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $transactions }}</div>
             </div>
             <div class="col-auto">
               <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
@@ -56,7 +56,7 @@
               <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Pending</div>
               <div class="row no-gutters align-items-center">
                 <div class="col-auto">
-                  <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">8</div>
+                  <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">{{ $transaction_pending }}</div>
                 </div>
               </div>
             </div>
@@ -75,7 +75,7 @@
           <div class="row no-gutters align-items-center">
             <div class="col mr-2">
               <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Sukses</div>
-              <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
+              <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $transaction_success }}</div>
             </div>
             <div class="col-auto">
               <i class="fas fa-check-circle fa-2x text-gray-300"></i>
